73a052d9fce17429b9922522a1ca6df0c033d713
Szymon Drozdowski <szymo.dr@gmail.com>
2023-04-02 18:14:03 +0200

    first commit

---
+++ src/components/List/List.js
@@ -0,0 +1,20 @@
+import Column from '../Column/Column';
+import styles from './List.module.scss'
+
+const List = () => {
+    return(
+        <div className={styles.list}>
+            <header className={styles.header}>
+                <h2 className={styles.title}>Things to do<span>soon</span></h2>
+            </header>
+            <p className={styles.description}>Intresting things i want to check out</p>
+            <section className={styles.columns}>
+                <Column title='Books' icon="book"/>
+                <Column title='Movies' icon="film"/>
+                <Column title='Games' icon="gamepad"/>
+            </section>
+        </div>
+    );
+};
+
+export default List;
