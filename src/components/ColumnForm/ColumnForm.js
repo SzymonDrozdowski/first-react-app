e05f02cac20cd185e83ead4f3b43eca4ea4e58eb
Szymon Drozdowski <szymo.dr@gmail.com>
2023-04-05 15:52:55 +0200

    add card

--- src/components/ColumnForm/ColumnForm.js
+++ src/components/ColumnForm/ColumnForm.js
@@ -6,6 +6,7 @@ import { useState } from 'react';
 const ColumnForm = props => {
     const [title, setTitle] = useState('');
     const [icon, setIcon] = useState('');
+    
     const handleSubmit = e => {
         e.preventDefault();
         props.action({ title: title, icon: icon });
@@ -16,9 +17,9 @@ const ColumnForm = props => {
 	return (
         <form className={styles.columnForm} onSubmit={handleSubmit}>
             <div>
-            <span><TextInput value={title} onChange={e => setTitle(e.target.value)} /></span>
-            <span><TextInput value={icon} onChange={e => setIcon(e.target.value)} /></span>
-            <Button>Add column</Button>
+                <span> Title:<TextInput value={title} onChange={e => setTitle(e.target.value)} /></span>
+                <span> Icon: <TextInput value={icon} onChange={e => setIcon(e.target.value)} /></span>
+                <Button>Add column</Button>
             </div>
         </form>
 	);
