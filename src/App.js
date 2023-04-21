f2229856102d364aaffe22f040c423a156732e55
Szymon Drozdowski <szymo.dr@gmail.com>
2023-04-21 20:13:02 +0200

    move Home to components

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import Container from './components/Container/Container';
 import NavBar from './components/NavBar/NavBar';
-import Home from './Home/Home';
-import About from './components/About/About'
-import Favorite from './components/Favorite/Favorite'
+import Home from './components/Home/Home';
+import About from './components/About/About';
+import Favorite from './components/Favorite/Favorite';
 import NotFound from './components/NotFound/NotFound';
 import List from './components/List/List'
 import { Routes, Route } from 'react-router-dom';
